6aa54c2b1f0d449aed8deec6728ea4afc6f604e0
obshi-desktop <obshi0120@icloud.com>
2018-07-14 01:50:29 +0900

    一方向のパーティクルに変更。

--- Assets/Scripts/NetworkParticleController.cs
+++ Assets/Scripts/NetworkParticleController.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 
 public class NetworkParticleController : MonoBehaviour {
 
+    public Shader shader;
+
     GameObject[] generators;
-	// Use this for initialization
-	void Start () {
+    Material material;
+
+    void Start () {
         generators = GameObject.FindGameObjectsWithTag("BaseObj");
-	}
+        material = new Material(shader);
+    }
 	
-	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void OnRenderObject()
+    {
+        for(int i = 0; i < generators.Length; i++)
+        {
+            ComputeBuffer particleBuffer = generators[i].GetComponent<NetworkParticleGenerator>().particleBuffer;
+            material.SetBuffer("particleBuffer", particleBuffer);
+            material.SetPass(0);
+            Graphics.DrawProcedural(MeshTopology.Points, particleBuffer.count);
+        }    
+    }
 }
--- Assets/Scripts/NetworkParticleGenerator.cs
+++ Assets/Scripts/NetworkParticleGenerator.cs
@@ -6,18 +6,20 @@ using System.Runtime.InteropServices;
 public struct Particle
 {
     public Vector3 pos;
+    public Vector3 start;
     public Vector3 end;
     public Vector3 vec;
     public float time;
     public float lifeTime;
     public int active;
 
-    public Particle(Vector3 pos, Vector3 end)
+    public Particle(Vector3 pos, Vector3 start,Vector3 end)
     {
         this.pos = pos;
+        this.start = start;
         this.end = end;
         this.vec = new Vector3(0,0,0);
-        time = 0f;
+        time = 0;
         lifeTime = 0;
         active = 0;
     }
@@ -25,25 +27,42 @@ public struct Particle
 
 public class NetworkParticleGenerator : MonoBehaviour
 {
-
     public GameObject[] destinations;
     public int NParticle = 100;
 
     public ComputeShader cpShader;
     public ComputeBuffer particleBuffer;
 
+    int updateKernel;
+    int emitKernel;
+    int initKernel;
+
     // Use this for initialization
     void Start()
     {
         InitBuffer();
+
+        initKernel = cpShader.FindKernel("Init");
+        emitKernel = cpShader.FindKernel("Emit");
+        updateKernel = cpShader.FindKernel("Update");
+
+        cpShader.SetBuffer(initKernel, "particles", particleBuffer);
+        cpShader.SetFloat("globalTime", Time.frameCount);
+        cpShader.Dispatch(initKernel, particleBuffer.count / 8 + 1, 1, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
+        cpShader.SetBuffer(updateKernel, "particles", particleBuffer);
+        cpShader.SetFloat("deltaTime", Time.deltaTime);
+        cpShader.Dispatch(updateKernel, particleBuffer.count / 8 + 1, 1, 1);
+
+        cpShader.SetBuffer(emitKernel, "particles", particleBuffer);
+        cpShader.Dispatch(emitKernel, particleBuffer.count / 8 + 1, 1, 1);
     }
 
-    public Vector3[] GetSender(){
+    public Vector3[] GetDestinations(){
         
         Vector3[] pos = new Vector3[destinations.Length];
 
@@ -56,14 +75,19 @@ public class NetworkParticleGenerator : MonoBehaviour
 
     void InitBuffer()
     {
-        particleBuffer = new ComputeBuffer(NParticle, Marshal.SizeOf(typeof(Particle)), ComputeBufferType.Default);
+        particleBuffer = new ComputeBuffer(NParticle, Marshal.SizeOf(typeof(Particle)));
 
         Particle[] particles = new Particle[particleBuffer.count];
-
+        
         for (int i = 0; i < particleBuffer.count; i++)
         {
-            particles[i] = new Particle(this.gameObject.transform.position, this.destinations[Random.Range(0, this.destinations.Length)].transform.position);
+            particles[i] = new Particle(this.gameObject.transform.position, this.gameObject.transform.position, this.destinations[Random.Range(0, this.destinations.Length)].transform.position);
         }
         particleBuffer.SetData(particles);
+    }
+
+    void OnDisable()
+    {
+        particleBuffer.Release();
     }
 }
